9ac10ae8ccf72a83ae6c50002df1a495c93be39a
Nelson David Daza <yankeenelson@gmail.com>
2021-10-29 17:24:56 -0700

    djnfjdng

--- main.js
+++ main.js
@@ -3,20 +3,27 @@
 
 //
 var $addEntryButton = document.querySelector('.entry-button');
-var $modal = document.querySelector('.modal');
+var $modal = document.querySelector('.background');
 var $container = document.querySelector('.container');
 var $submitButton = document.querySelector('[type="submit"]');
 var $date = document.querySelector('select');
 var $time = document.querySelector('[type="time"]');
 var $description = document.querySelector('textarea');
 var $form = document.querySelector('form');
+var $tbody = document.querySelector('tbody');
+var $table = document.querySelector('.scheduled-events');
+var $weekDayButtons = document.querySelector('.weekdays');
+
 //
 $addEntryButton.addEventListener('click', modalOn);
 //
 function modalOn(event) {
-  $container.className = 'container background';
-  $modal.className = 'modal center';
+
+  $modal.className = 'background';
   $addEntryButton.className = 'entry-button hidden';
+  $table.className = 'column-full scheduled-events hidden';
+  $weekDayButtons.className = 'column-full justify-space-evenly weekdays hidden';
+
 }
 $form.addEventListener('submit', submitHandler);
 //
@@ -28,9 +35,28 @@ function submitHandler(event) {
     description: $description.value,
     entryId: data.nextEntryId++
   };
+  var renderedObject = renderTable(dataObj);
+  $tbody.prepend(renderedObject);
   data.entries.unshift(dataObj);
   $container.className = 'container';
   $modal.className = 'modal center hidden';
   $addEntryButton.className = 'entry-button';
+
 }
 //
+function renderTable(entry) {
+
+  var $tr = document.createElement('tr');
+  var $tdTime = document.createElement('td');
+  var $tdDescription = document.createElement('td');
+  //
+  $tdTime.textContent = entry.time;
+  $tdDescription.textContent = entry.description;
+
+  //
+  $tr.appendChild($tdTime);
+  $tr.appendChild($tdDescription);
+
+  return $tr;
+
+}
